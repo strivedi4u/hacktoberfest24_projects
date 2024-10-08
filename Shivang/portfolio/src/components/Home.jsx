b111d5a94a0797e5f211012ae3987da585ac10e7
Sowmik Dey <sowmikdey23@gmail.com>
2024-10-08 16:22:18 +0530

    Portfolio made but only the home page.I'll raise 3 more issues where I will edit the remaining pages.

---
+++ Shivang/portfolio/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="app">
+   <h1 className="intro" >
+   Software Developer <br /> & Tech Enthusiast.
+   </h1>
+    <p className='prsn-dtls'>
+    I'm Shivang Trivedi, Fullstack developer/software engineer. <br /> I am on my journey to exploring the world of Softwares and AI. 
+    </p>
+    <div className='dp'>
+      <img className="image-3d" src="/public/MESSI.jpg" alt="" />
+    </div>
+    </div>
+  )
+}
+
+
+export default Home
